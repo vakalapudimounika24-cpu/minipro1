7212fbdefa46f0772bc5995134b34e6e080338d3
Vakalapudi Mounika <vakalapudimounika24@gmail.com>
2026-06-19 15:48:12 +0530

    This my project

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    alert(`Login Successful\nEmail: ${email}`);
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleLogin}>
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+
+        <p>
+          Don't have an account? <span>Sign Up</span>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default App;
